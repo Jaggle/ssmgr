b87260ee804cc99d8aff63e10eaa3a5f4dddd98d
Jake <singviy@gmail.com>
2017-11-28 13:28:39 +0800

    add command.js

---
+++ command.js
@@ -0,0 +1,31 @@
+if(process.env.NODE_ENV !== 'production') {
+  console.log('use babel-core/register');
+  require('babel-core/register');
+}
+
+require('./init/log');
+
+const log4js = require('log4js');
+const logger = log4js.getLogger('system');
+
+process.on('unhandledRejection', (reason, p) => {
+  logger.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
+process.on('uncaughtException', (err) => {
+  logger.error(`Caught exception: ${err}`);
+});
+
+require('./init/utils');
+
+require('./init/moveConfigFile');
+require('./init/checkConfig');
+require('./init/knex');
+const manager = require('./services/manager');
+
+manager.send({command: 'version'}, {
+    host: '127.0.0.1',
+    port: 4001
+}).then(success => {
+    console.log(success);
+});
