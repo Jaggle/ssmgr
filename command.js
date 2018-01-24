ef3566990954ce4cd576c1b6b1d7de73bf264445
Jaggle <singviy@gmail.com>
2018-01-24 09:38:08 +0800

    update

--- command.js
+++ command.js
@@ -21,7 +21,45 @@ require('./init/utils');
 require('./init/moveConfigFile');
 require('./init/checkConfig');
 require('./init/knex');
-const manager = require('./services/manager');
+
+//const manager = require('./services/manager');
+
+
+const initDb = require('./init/loadModels').init;
+
+initDb().then(() => {
+  const knex = appRequire('init/knex').knex;
+  const email = appRequire('plugins/email/index');
+
+  const sendMail = async () => {
+    const allEmail = await knex('user').select(['email']);
+    const notice = await knex('email_notice').select();
+    const title = notice[0].title;
+    const content = notice[0].content;
+
+    let index = 0;
+    let interval = setInterval(() => {
+      if (index < allEmail.length) {
+        let _email = allEmail[index++];
+        email.sendMail(_email.email, title, content, {
+          type: 'email-notice',
+        });
+        console.log('send email for: ' + _email.email);
+      } else {
+        clearInterval(interval);
+        console.log('finished!');
+      }
+    }, 3000);
+  };
+
+  sendMail();
+}).then(() => {
+
+
+}).catch(err => {
+  logger.error(err);
+});
+
 
 // manager.send({command: 'version'}, {
 //     host: '45.32.123.118',
@@ -32,10 +70,12 @@ const manager = require('./services/manager');
 // });
 
 
-manager.send({command: 'add', port: 29756, password: '9432229077'}, {
-    host: 'hk1.v.greentern.net',
-    port: 4001,
-    password: '1991JakeSS`'
-}).then(success => {
-    console.log(success);
-});
+// manager.send({command: 'add', port: 29756, password: '9432229077'}, {
+//     host: 'hk1.v.greentern.net',
+//     port: 4001,
+//     password: '1991JakeSS`'
+// }).then(success => {
+//     console.log(success);
+// });
+
+
